c87ae30c1653f84e46505d0f2a379ae6cb9fd6e4
Ara-O <oladipoeyiara@gmail.com>
2021-09-13 10:50:06 -0400

    Made app icons react on hover 🎇 and bottom taskbar style changes

--- src/views/homescreen.vue
+++ src/views/homescreen.vue
@@ -14,130 +14,108 @@
         class="searchinput"
         placeholder="Type here to search"
       />
+      <!-- APP ICON SECTION -->
       <div class="appsicon">
-        <img
-          src="../assets/appicons/cortanaicon.png"
-          alt="Cortana logo"
-          class="cortana taskbaricon"
-        />
-        <img
-          src="../assets/appicons/taskbar.png"
-          alt="Taskbar"
-          class="taskbaricon"
-        />
-        <img
-          src="../assets/appicons/fileexplorer.png"
-          alt="File Explorer"
-          class="taskbaricon"
-          style="width: 26px"
-        />
-        <img
-          src="../assets/appicons/chromeicon.png"
-          alt="Chrome icon"
-          class="taskbaricon"
-          style="width: 25px"
-        />
-        <img
-          src="../assets/appicons/outlookicon.png"
-          alt="Outlook icon"
-          class="taskbaricon"
-          style="width: 25px"
-        />
-        <img
-          src="../assets/appicons/visualstudioicon.png"
-          alt="Visual studio code icon"
-          class="taskbaricon"
-          style="width: 25px"
-        />
+        <div class="icon" >
+          <img
+            src="../assets/appicons/cortanaicon.png"
+            alt="Cortana logo"
+            class="cortana taskbaricon"
+          />
+        </div>
+        <div class="icon">
+          <img
+            src="../assets/appicons/taskbar.png"
+            alt="Taskbar"
+            class="taskbaricon"
+          />
+        </div>
+        <div class="icon">
+          <img
+            src="../assets/appicons/fileexplorer.png"
+            alt="File Explorer"
+            class="taskbaricon"
+            style="width: 26px"
+          />
+        </div>
+        <div class="icon">
+          <img
+            src="../assets/appicons/chromeicon.png"
+            alt="Chrome icon"
+            class="taskbaricon"
+            style="width: 25px"
+          />
+        </div>
+        <div class="icon">
+          <img
+            src="../assets/appicons/outlookicon.png"
+            alt="Outlook icon"
+            class="taskbaricon"
+            style="width: 25px"
+          />
+        </div>
+        <div class="icon">
+          <img
+            src="../assets/appicons/visualstudioicon.png"
+            alt="Visual studio code icon"
+            class="taskbaricon"
+            style="width: 25px"
+          />
+        </div>
       </div>
+
+      <div style="display: flex; align-items: center; margin-left: 30px">
+        <div class="utilityicon">
+          <img
+            src="@/assets/appicons/upicon.png"
+            alt="Up icon"
+            class="taskbaricon2"
+            style="width: 15px"
+          />
+        </div>
+        <div class="utilityicon">
+          <img
+            src="@/assets/appicons/onedrive.png"
+            alt="Onedrive icon"
+            class="taskbaricon2"
+            style="width: 20px"
+          />
+        </div>
+        <div class="utilityicon">
+          <img
+            src="@/assets/appicons/wifi.png"
+            alt="Wifi icon"
+            class="taskbaricon2"
+            style="width: 23px; transform: rotate(315deg); padding-top: 9px"
+          />
+        </div>
+        <div class="utilityicon">
+          <img
+            src="@/assets/appicons/speakericon.png"
+            alt="Speaker icon"
+            class="taskbaricon2"
+            style="width: 18px"
+          />
+        </div>
+        <div style="display: flex; flex-direction: column">
+          <h5 class="mini-date">2:11 PM</h5>
+          <h5 class="mini-date">9/12/2021</h5>
+        </div>
+        <div class="utilityicon">
+          <img
+            src="@/assets/appicons/notifications.png"
+            alt="Notification icon"
+            class="taskbaricon2"
+            style="width: 39px"
+          />
+        </div>
+      </div>
+      <div class="removeall"></div>
     </footer>
   </main>
 </template>
 
-<style scoped>
-main {
-  background: url(../assets/homescreenbg.jpg);
-  height: 100vh;
-  width: 100vw;
-  overflow: hidden;
-  background-size: 100%;
-  margin: 0px;
-  filter: brightness(0);
-  transition: filter 500ms ease-in-out;
-}
-
-footer {
-  display: grid;
-  grid-template-columns: 48px 344px 1fr;
-  background: rgb(138 138 138 / 24%);
-  position: absolute;
-  bottom: 1px;
-  height: 45px;
-  width: 99vw;
-  overflow: hidden;
-}
-
-img.windowsicon {
-  width: 22px;
-}
-
-.windowsicondiv {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.searchinput {
-  background: #383838;
-  font-family: "Segoe UI Light";
-  border-radius: 0px;
-  font-weight: 200;
-  font-size: 15.5px;
-  padding-left: 39px;
-  color: white;
-  border: solid 0.5px rgb(102, 102, 102);
-}
-
-.searchinput:focus {
-  outline: none;
-}
-
-.searchinput::placeholder {
-  color: white;
-}
-
-.searchinput:hover {
-  border: solid 3px rgb(107, 107, 107);
-  background: #4b4b4b;
-}
-
-.taskbaricon {
-  width: 17px;
-}
-
-.cortana {
-  filter: blur(0.3px);
-  margin-left: 16px;
-}
-
-.appsicon {
-  display: flex;
-  align-items: center;
-  column-gap: 31px;
-}
-
-.windowsicondiv:hover {
-  background: #323232b0;
-}
-
-img.search {
-  width: 18px;
-  position: absolute;
-  left: 61px;
-  transform: rotate(90deg);
-  top: 16px;
-}
-</style>
+<style scoped src="@/styles/homescreen.css"></style>
 
 <script>
 export default {
@@ -167,6 +145,23 @@ export default {
     setTimeout(function () {
       document.querySelector("main").style.filter = "brightness(1)";
     }, 50);
+
+    document
+      .querySelector("footer")
+      .addEventListener("mouseover", function (e) {
+        const clicked = e.target;
+        if (clicked.classList.contains("taskbaricon")) {
+          clicked.closest(".icon").style.background = "#80808063";
+        }
+      });
+
+    document.querySelectorAll(".taskbaricon").forEach((element) => {
+      element.addEventListener("mouseleave", function () {
+        element.closest(".icon").style.background = "transparent";
+      });
+    });
+
+   
   },
 };
 </script>
