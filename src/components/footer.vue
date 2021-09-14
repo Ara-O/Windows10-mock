32e24706a4cac9759071e842805d2ae1307774b0
Ara-O <oladipoeyiara@gmail.com>
2021-09-14 10:08:37 -0400

    Started working on start menu

---
+++ src/components/footer.vue
@@ -0,0 +1,202 @@
+<template>
+  <section class="startmenu">
+    <div>
+      <img
+        src="../assets/appicons/hamburgericon.png"
+        alt="Hamburger icon"
+        class="hamburgericon"
+      />
+      <div class="bottomicons">
+        <img
+          src="@/assets/appicons/settingsicon.png"
+          alt="Settings Icon"
+          class="startmenuicon"
+        />
+        <img
+          src="@/assets/appicons/shutdownicon.png"
+          alt="Shutdown Icon"
+          class="startmenuicon"
+        />
+      </div>
+    </div>
+    <div></div>
+    <div></div>
+  </section>
+  <footer>
+    <div class="windowsicondiv">
+      <img :src="windowsicon" alt="Windows Icon" class="windowsicon" />
+    </div>
+    <img
+      src="@/assets/appicons/searchicon.png"
+      alt="Search icon"
+      class="search"
+    />
+    <input type="text" class="searchinput" placeholder="Type here to search" />
+    <!-- APP ICON SECTION -->
+    <div class="appsicon">
+      <div class="icon">
+        <img
+          src="../assets/appicons/cortanaicon.png"
+          alt="Cortana logo"
+          class="cortana taskbaricon"
+        />
+      </div>
+      <div class="icon">
+        <img
+          src="../assets/appicons/taskbar.png"
+          alt="Taskbar"
+          class="taskbaricon"
+          title="Task view"
+        />
+      </div>
+      <div class="icon">
+        <img
+          src="../assets/appicons/fileexplorer.png"
+          alt="File Explorer"
+          class="taskbaricon"
+          style="width: 26px"
+          title="File Explorer"
+        />
+      </div>
+      <div class="icon">
+        <img
+          src="../assets/appicons/chromeicon.png"
+          alt="Chrome icon"
+          class="taskbaricon"
+          style="width: 25px"
+          title="Chrome"
+        />
+      </div>
+      <div class="icon">
+        <img
+          src="../assets/appicons/outlookicon.png"
+          alt="Outlook icon"
+          class="taskbaricon"
+          style="width: 25px"
+          title="Mail"
+        />
+      </div>
+      <div class="icon">
+        <img
+          src="../assets/appicons/visualstudioicon.png"
+          alt="Visual studio code icon"
+          class="taskbaricon"
+          style="width: 25px"
+          title="Visual Studio Code"
+        />
+      </div>
+    </div>
+
+    <div
+      class="utilityicondiv"
+      style="display: flex; align-items: center; margin-left: 30px"
+    >
+      <div class="utilityicon">
+        <img
+          src="@/assets/appicons/upicon.png"
+          alt="Up icon"
+          class="taskbaricon2"
+          style="width: 15px"
+          title="Show hidden icons"
+        />
+      </div>
+      <div class="utilityicon">
+        <img
+          src="@/assets/appicons/onedrive.png"
+          alt="Onedrive icon"
+          class="taskbaricon2"
+          style="width: 20px"
+          title="Onedrive"
+        />
+      </div>
+      <div class="utilityicon">
+        <img
+          src="@/assets/appicons/wifi.png"
+          alt="Wifi icon"
+          class="taskbaricon2"
+          style="width: 23px; transform: rotate(315deg); padding-top: 9px"
+          title="udmsecure"
+        />
+      </div>
+      <div class="utilityicon">
+        <img
+          src="@/assets/appicons/speakericon.png"
+          alt="Speaker icon"
+          class="taskbaricon2"
+          style="width: 18px"
+          title="Headphones ( Bluetooth ): 455"
+        />
+      </div>
+      <div
+        style="
+          display: flex;
+          flex-direction: column;
+          width: 66px;
+          padding: 0px 5px;
+        "
+        class="utilityicon"
+      >
+        <h5 class="mini-date">2:11 PM</h5>
+        <h5 class="mini-date">9/12/2021</h5>
+      </div>
+      <div class="utilityicon">
+        <img
+          src="@/assets/appicons/notifications.png"
+          alt="Notification icon"
+          class="taskbaricon2"
+          style="width: 39px"
+          onmouseover="this.style.background='rgb(128 128 128 / 20%)'"
+          onmouseleave="this.style.background='transparent'"
+        />
+      </div>
+    </div>
+    <div class="removeall"></div>
+  </footer>
+</template>
+
+<style scoped src="../styles/homescreen.css"></style>
+<style scoped>
+.startmenu {
+  height: 630px;
+  width: 640px;
+  position: relative;
+  left: 0px;
+  bottom: -630px;
+  transition: bottom 200ms linear, background 4s linear;
+  display: grid;
+  grid-template-columns: 45px 1fr 1fr;
+  background: transparent;
+  filter: brightness(0);
+}
+
+.position {
+  bottom: 45px;
+  background: linear-gradient(rgb(53 53 53 / 94%), #272728);
+  filter: brightness(1);
+}
+
+.hamburgericon {
+  left: 15px;
+  top: 15px;
+  width: 19px;
+  position: absolute;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return { windowsicon: require("../assets/appicons/windowsicon.png") };
+  },
+
+  mounted() {
+    document
+      .querySelector(".windowsicondiv")
+      .addEventListener("click", function () {
+        console.log("potata");
+        document.querySelector(".startmenu").classList.toggle("position");
+        // document.querySelector(".startmenu").style.bottom = "20px";
+      });
+  },
+};
+</script>
--- src/views/homescreen.vue
+++ src/views/homescreen.vue
@@ -1,164 +1,33 @@
 <template>
   <main>
     <div class="homescreenicon">
-      <img src="@/assets/appicons/trashicon.png" alt="Trashicon" />
+      <img
+        src="@/assets/appicons/trashicon.png"
+        alt="Trashicon"
+        title="Contains files and folders that you have deleted"
+      />
       <h5>Recycle Bin</h5>
     </div>
-    <footer>
-      <div class="windowsicondiv">
-        <img :src="windowsicon" alt="Windows Icon" class="windowsicon" />
-      </div>
-      <img
-        src="@/assets/appicons/searchicon.png"
-        alt="Search icon"
-        class="search"
-      />
-      <input
-        type="text"
-        class="searchinput"
-        placeholder="Type here to search"
-      />
-      <!-- APP ICON SECTION -->
-      <div class="appsicon">
-        <div class="icon">
-          <img
-            src="../assets/appicons/cortanaicon.png"
-            alt="Cortana logo"
-            class="cortana taskbaricon"
-          />
-        </div>
-        <div class="icon">
-          <img
-            src="../assets/appicons/taskbar.png"
-            alt="Taskbar"
-            class="taskbaricon"
-          />
-        </div>
-        <div class="icon">
-          <img
-            src="../assets/appicons/fileexplorer.png"
-            alt="File Explorer"
-            class="taskbaricon"
-            style="width: 26px"
-          />
-        </div>
-        <div class="icon">
-          <img
-            src="../assets/appicons/chromeicon.png"
-            alt="Chrome icon"
-            class="taskbaricon"
-            style="width: 25px"
-          />
-        </div>
-        <div class="icon">
-          <img
-            src="../assets/appicons/outlookicon.png"
-            alt="Outlook icon"
-            class="taskbaricon"
-            style="width: 25px"
-          />
-        </div>
-        <div class="icon">
-          <img
-            src="../assets/appicons/visualstudioicon.png"
-            alt="Visual studio code icon"
-            class="taskbaricon"
-            style="width: 25px"
-          />
-        </div>
-      </div>
-
-      <div style="display: flex; align-items: center; margin-left: 30px">
-        <div class="utilityicon">
-          <img
-            src="@/assets/appicons/upicon.png"
-            alt="Up icon"
-            class="taskbaricon2"
-            style="width: 15px"
-          />
-        </div>
-        <div class="utilityicon">
-          <img
-            src="@/assets/appicons/onedrive.png"
-            alt="Onedrive icon"
-            class="taskbaricon2"
-            style="width: 20px"
-          />
-        </div>
-        <div class="utilityicon">
-          <img
-            src="@/assets/appicons/wifi.png"
-            alt="Wifi icon"
-            class="taskbaricon2"
-            style="width: 23px; transform: rotate(315deg); padding-top: 9px"
-          />
-        </div>
-        <div class="utilityicon">
-          <img
-            src="@/assets/appicons/speakericon.png"
-            alt="Speaker icon"
-            class="taskbaricon2"
-            style="width: 18px"
-          />
-        </div>
-        <div style="display: flex; flex-direction: column">
-          <h5 class="mini-date">2:11 PM</h5>
-          <h5 class="mini-date">9/12/2021</h5>
-        </div>
-        <div class="utilityicon">
-          <img
-            src="@/assets/appicons/notifications.png"
-            alt="Notification icon"
-            class="taskbaricon2"
-            style="width: 39px"
-          />
-        </div>
-      </div>
-      <div class="removeall"></div>
-    </footer>
+    <taskbar></taskbar>
   </main>
 </template>
 
 <style scoped src="@/styles/homescreen.css"></style>
 
 <script>
+import taskbar from "../components/footer";
 import interact from "interactjs";
 export default {
+  components: {
+    taskbar,
+  },
+
   data() {
-    return {
-      windowsicon: require("../assets/appicons/windowsicon.png"),
-    };
+    return {};
   },
 
   mounted() {
-    // var element = document.querySelector(".homescreenicon");
-    // var x = 0;
-    // var y = 0;
-
-    // interact(element)
-    //   .draggable({
-    //     modifiers: [
-    //       interact.modifiers.snap({
-    //         targets: [interact.snappers.grid({ x: 60, y: 60 })],
-    //         range: Infinity,
-    //         relativePoints: [{ x: 0, y: 0 }],
-    //       }),
-    //       interact.modifiers.restrict({
-    //         restriction: element.parentNode,
-    //         elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
-    //         endOnly: true,
-    //       }),
-    //     ],
-    //     inertia: true,
-    //   })
-    //   .on("dragmove", function (event) {
-    //     x += event.dx;
-    //     y += event.dy;
-
-    //     event.target.style.transform = "translate(" + x + "px, " + y + "px)";
-    //   });
     const position = { x: 0, y: 0 };
-
     interact(".homescreenicon").draggable({
       listeners: {
         start(event) {
@@ -197,14 +66,28 @@ export default {
       .querySelector("footer")
       .addEventListener("mouseover", function (e) {
         const clicked = e.target;
-        if (clicked.classList.contains("taskbaricon")) {
+        if (clicked.classList.contains("icon")) {
           clicked.closest(".icon").style.background = "#80808063";
         }
       });
 
-    document.querySelectorAll(".taskbaricon").forEach((element) => {
+    document.querySelectorAll(".icon").forEach((element) => {
       element.addEventListener("mouseleave", function () {
         element.closest(".icon").style.background = "transparent";
+      });
+    });
+
+    document
+      .querySelector(".utilityicondiv")
+      .addEventListener("mouseover", function (e) {
+        if (e.target.classList.contains("utilityicon")) {
+          e.target.style.background = "rgb(128 128 128 / 24%)";
+        }
+      });
+
+    document.querySelectorAll(".utilityicon").forEach((element) => {
+      element.addEventListener("mouseleave", function () {
+        element.style.background = "transparent";
       });
     });
   },
