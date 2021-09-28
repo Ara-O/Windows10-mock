5b94c331cdc3296dc4f1fba5b279ec534ac49ba4
Ara-O <oladipoeyiara@gmail.com>
2021-09-28 10:50:27 -0400

    Worked on Notifications menu 🎉

--- src/components/notifications.vue
+++ src/components/notifications.vue
@@ -7,10 +7,45 @@
         alt="Alert Icon"
         class="alerticon"
       />
-      <h5 class="notif--message">
-        Your file history was disconnected for too long. Reconnect in order to
-        keep packing up your files or else data might be lost.
-      </h5>
+      <div>
+        <h4 class="reconnectdrive">Reconnect your drive</h4>
+        <h5 class="notif--message">
+          Your file history was disconnected for too long. Reconnect it and tap
+          or click to keep saving copies of your files.
+        </h5>
+        <h6 class="time">9:02 PM</h6>
+      </div>
+    </div>
+    <div class="bottomtoggles">
+      <div class="bottomtogglessection">
+        <h5 class="managenotifs2">Expand</h5>
+        <h5 class="managenotifs3">Clear all notifications</h5>
+      </div>
+      <div class="toggles">
+        <toggle
+          name="Bluetooth"
+          :image="require('../assets/appicons/bluetoothicon.png')"
+          iconwidth="18px"
+        ></toggle>
+        <toggle
+          name="Night Light"
+          :image="require('../assets/appicons/brightnessicon.png')"
+          iconwidth="24px"
+          defaults="-2px"
+        ></toggle>
+        <toggle
+          name="Location"
+          :image="require('../assets/appicons/location.png')"
+          iconwidth="18px"
+          defaults="1px"
+        ></toggle>
+        <toggle
+          name="Focus Assist"
+          :image="require('../assets/appicons/focusassist.png')"
+          iconwidth="18px"
+          defaults="1px"
+        ></toggle>
+      </div>
     </div>
   </div>
 </template>
@@ -34,7 +69,7 @@
 .managenotifs {
   position: absolute;
   font-size: 12px;
-  top: 39px;
+  top: 41px;
   right: 27px;
   font-weight: 100;
   font-family: sans-serif;
@@ -49,20 +84,20 @@
 .notification {
   display: flex;
   margin-top: 140px;
-  background: #1d1d1d;
-  align-items: center;
+  background: #2a2a2a;
+  align-items: flex-start;
   justify-content: center;
-  margin: 140px 17px;
-  height: 159px;
+  margin: 92px 14px;
 }
 
 .notification:hover {
-  background: #3f3d3da8;
+  background: linear-gradient(130deg, #363535a8, #2a2a2a);
 }
 
 .alerticon {
-  width: 136px;
-  filter: hue-rotate(317deg);
+  width: 128px;
+  filter: hue-rotate(343deg);
+  margin-left: -24px;
 }
 
 .notif--message {
@@ -71,5 +106,84 @@
   line-height: 21px;
   width: 202px;
   font-weight: 100;
+  font-size: 14px;
+  margin: -18px 0px -1px;
+}
+
+.reconnectdrive {
+  color: white;
+  font-family: "Segoe UI";
+  font-weight: 100;
+}
+
+.bottomtoggles {
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+}
+
+.bottomtogglessection {
+  display: flex;
+  justify-content: space-between;
+}
+
+.managenotifs2 {
+  font-family: "Segoe Ui Light";
+  font-weight: 100;
+  color: white;
+  font-size: 12px;
+  color: #8ddaff;
+  padding-left: 15px;
+}
+
+.managenotifs3 {
+  font-size: 12px;
+  color: #8ddaff;
+  right: 0px;
+  font-family: "Segoe UI Light";
+  font-weight: 100;
+  padding-right: 15px;
+}
+
+.managenotifs2:hover,
+.managenotifs3:hover {
+  color: white;
+  cursor: default;
+}
+.time {
+  color: white;
+  font-size: 11px;
+  font-family: "Segoe Ui Light";
+  font-weight: 100;
+  margin: 11px 0px 22px;
+}
+
+.toggles {
+  display: flex;
+  justify-content: center;
+  column-gap: 7px;
+  position: relative;
+}
+
+.toggle {
+  background: #484848;
+  height: 71px;
+  width: 88px;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+  padding: 7px;
+}
+
+.toggle:hover {
+  background: linear-gradient(325deg, #484848, #565656);
 }
 </style>
+
+<script>
+import toggle from "../components/toggles.vue";
+export default {
+  components: {
+    toggle,
+  },
+};
+</script>
---
+++ src/components/toggles.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="toggle">
+    <img
+      :src="image"
+      alt="Bluetooth Icon"
+      class="toggleimage"
+      :style="{ width: iconwidth, marginTop: defaults }"
+    />
+    <h3 style="color: white" class="togglename">{{ name }}</h3>
+  </div>
+</template>
+<script>
+export default {
+  props: ["name", "image", "iconwidth", "defaults"],
+};
+</script>
+
+<style scoped>
+.toggleimage {
+  width: 18px;
+  margin-left: -3px;
+}
+
+.togglename {
+  font-size: 13px;
+  font-family: "Segoe Ui Light";
+  font-weight: 100;
+  margin-top: 15px;
+  position: absolute;
+  bottom: 4px;
+}
+</style>
